e16538f494b7230c4c2571d8f83a3aca67908f7a
Gustavo R Martins <gustavo.roube.martins@hotmail.com>
2024-07-12 19:37:29 -0300

    Att. ex12

--- ex12.php
+++ ex12.php
@@ -8,13 +8,23 @@
 </head>
 <body>
     <main>
-        <form action="<?php echo $_SERVER=['PHP_SELF']?>" method="get">Soma
+        <form action="ex12.php" method="post">Soma
             <label for="valor1">Insira o primeiro valor</label>
-            <input type="number" name="valor1" id="idvalor1">
+            <input type="number" name="valor1" id="idvalor1" step="0.01">
             <label for="valor2">Insira o segundo valor</label>
-            <input type="number" name="valor2" id="idvalor2">
+            <input type="number" name="valor2" id="idvalor2" step="0.01">
             <input type="submit" value="Calcular">
         </form>
+        <?php
+        $variavel1= $_POST["valor1"]?:0; 
+        $variavel2= $_POST["valor2"]?:0; 
+        $soma = $variavel1 + $variavel2;
+        $formato = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+
+        echo   "<p>a soma entre os números é ". numfmt_format_currency($formato, $soma, "BRL") . "</p>"
+
+
+        ?>
     </main>
 </body>
 </html>
