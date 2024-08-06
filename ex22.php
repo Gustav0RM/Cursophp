8540614246344cceafaf160175c3fa079dae15ab
Gustavo R Martins <gustavo.roube.martins@hotmail.com>
2024-08-06 20:55:45 -0300

    Att
    Estrutura condicional switch, com select html

---
+++ ex22.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Switch</title>
+</head>
+
+<body>
+    <main>
+        <form action="ex22.php" method="post">Calculos
+            <label for="valor1">Insira o primeiro número</label>
+            <input type="number" name="valor1" id="valor1">
+            <label for="valor2">Insira o segundo número</label>
+            <input type="number" name="valor2" id="valor2">
+            <label for="valor3">Insira o terceiro número</label>
+            <input type="number" name="valor3" id="valor3">
+            <label for="operações">Operação</label>
+            
+            <select name="opções" id="opções">
+                <option value="1" selected hidden>--Selecione--</option>
+                <option value="2">Soma</option>
+                <option value="3">Subtração</option>
+                <option value="4">Divisão</option>
+                <option value="5">Multiplicação</option>
+            </select>
+            <input type="submit" value="Calcular">
+        </form>
+    </main>
+
+    <?php
+    $valor1 = $_POST["valor1"] ?: 0;
+    $valor2 = $_POST["valor2"] ?: 0;
+    $valor3 = $_POST["valor3"] ?: 0;
+    $opções = $_POST["opções"];
+    ?>
+    <article>
+        <?php
+        switch ($opções) {
+            case 1:
+                echo "<p>Selecione uma operação</p>";
+                break;
+            case 2:
+                echo "<p>A soma é: " . $valor1 + $valor2 + $valor3 . "</p>";
+                break;
+            case 3:
+                echo "<p>A subtração é: " . $valor1 - $valor2 - $valor3 . "</p>";
+                break;
+            case 4:
+                echo "<p>A divisão é: " . $valor1 / $valor2 / $valor3 . "</p>";
+                break;
+            case 5:
+                echo "<p>A multiplicação é: " . $valor1 * $valor2 * $valor3 . "</p>";
+        }
+        ?>
+    </article>
+</body>
+
+</html>
