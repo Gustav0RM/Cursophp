207b5c87358d1bd8d6ba6aaaed03ed453096375e
Gustavo R Martins <gustavo.roube.martins@hotmail.com>
2024-08-26 16:56:05 -0300

    update
    Correção identação

--- ex30.php
+++ ex30.php
@@ -6,23 +6,28 @@
     <link rel="stylesheet" href="style.css">
     <title>Estruturas de repetição</title>
 </head>
+
+<!-- Versão do exercício anterior utilizando o FOR -->
+
 <body>
-  <main>                                           <!-- Versão do exercício anterior utilizando o FOR -->
+  <main>                                           
     <form action="ex30.php" method="post">Identificando Números<?php
-  for ($count_A = 1; $count_A <= 5; $count_A++)
-    { ?>
-    <label for="numero<?=$count_A?>">Insira um número</label>
-    <input type="number" name="numero<?=$count_A?>" id="numero<?=$count_A?>"> <?php
-    } ?>
-    <input type="submit" value="Enviar">
+      for ($count_A = 1; $count_A <= 5; $count_A++)
+        { ?>
+        <label for="numero<?=$count_A?>">Insira um número</label>
+        <input type="number" name="numero<?=$count_A?>" id="numero<?=$count_A?>"> <?php
+        } ?>
+      <input type="submit" value="Enviar">
     </form>
+
     <p class="navega">
       <a href="../Cursophp/ex31.php">Avançar</a>
       <a href="../Cursophp/ex29.php">Retornar</a><br>
       <a href="../Cursophp/">Index</a>
-
     </p>
+
   </main> <?php 
+  
   if (isset($_POST["numero1"])) 
   //condição utilizada para não exibir erro enquanto não for enviado nada ao servidor
     {
