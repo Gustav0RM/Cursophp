06305ce5f3e435a399d16d03f605f1b1325e0248
Gustavo R Martins <gustavo.roube.martins@hotmail.com>
2024-09-17 17:05:11 -0300

    att

--- ex36.php
+++ ex36.php
@@ -4,35 +4,49 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <title>ARRAY</title>
 </head>
+
 <body>
   <main>
-    <form action="ex36.php" method="post"><?php
-    for ($count = 0; $count < 4 ; $count++)
-      {?>
-      <label for="nome">Nome</label>
-      <input type="text" name="memoria[]" id="nome">
-      <label for="sobrenome">Sobrenome</label>
-      <input type="text" name="memoria[]" id="sobrenome">
-      <label for="idade">Idade</label>
-      <input type="number" name="memoria[]" id="idade"><hr> <?php 
-      }?>
+    <form action="ex36.php" method="post">
+      <div class="row m-0"><?php
+        for ($count = 1; $count < 4; $count++)
+          {?>
+          <div class="col-sm-4">
+            <label for="nome">Nome</label>
+            <input type="text" name="memoria[]" id="nome">
+            <label for="sobrenome">Sobrenome</label>
+            <input type="text" name="memoria[]" id="sobrenome">
+            <label for="idade">Idade</label>
+            <input type="number" name="memoria[]" id="idade">
+          </div> <?php
+          } ?>
       <input type="submit" value="Enviar">
+      </div><!--final da linha -->
     </form>
-  </main>
+  </main> 
+  <table> <?php
 
-  <article><?php
-        $memoria = $_POST['memoria'];
-        foreach ($memoria as $id => $conteudo)
-          {
-          echo "<table>
-                <td>$conteudo</td>
-                </table>";
-          }?>
-  </article>
+    $memoria = $_POST['memoria'];
+    foreach ($memoria as $id => $dados)
+      {
+      echo "<tr>";
+      foreach ($memoria[$id] as $cedula)
+        {
+        echo "<td>". $cedula. "</td>";
+        }
+      echo "</tr>";
+      } ?>
+  </table>
     
 
-  
+
+
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
